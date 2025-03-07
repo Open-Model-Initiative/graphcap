e49d7f420cdda0456ac30f7667dfc3c837f18d53
jphillips <josh.phillips@fearnworks.com>
2025-03-07 10:07:34 -0600

    Remove old pagination
    Signed-off-by: jphillips <josh.phillips@fearnworks.com>

--- graphcap_studio/src/features/editor/components/image-viewer/carousel/index.ts
+++ graphcap_studio/src/features/editor/components/image-viewer/carousel/index.ts
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: Apache-2.0
 export { CarouselViewer } from './CarouselViewer';
-export { Pagination } from './Pagination';
 export { ThumbnailStrip } from './ThumbnailStrip';
 export * from './hooks'; 
