60b8e27ad744a32368cc7013f638286d70e39fdd
jphillips <josh.phillips@fearnworks.com>
2025-03-08 03:37:46 -0600

    Add link to default gallery on index page
    Signed-off-by: jphillips <josh.phillips@fearnworks.com>

--- graphcap_studio/src/routes/index.tsx
+++ graphcap_studio/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -6,8 +6,14 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
+    <div className="p-2 text-white">
       <h3>Welcome to graphcap Studio!</h3>
+      <Link 
+            to="/gallery/os_img" 
+            className="px-2 py-1 rounded-md text-xs text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-800"
+          >
+            Go to Default Gallery
+          </Link>
     </div>
   )
 }
